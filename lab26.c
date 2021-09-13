61b73ab2bdcc7d5a78a843f632d5d7b4c78e6def
Tanya <70838088+tkubysheva@users.noreply.github.com>
2021-09-13 14:56:02 +0700

    Create lab26.c

---
+++ lab26.c
@@ -0,0 +1,100 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <ctype.h>
+#include <wait.h>
+
+#define MAXBUFF 64
+
+int printStat(int stat)
+{
+        if(WIFEXITED(stat))
+        {
+                int exitCode = WEXITSTATUS(stat);
+                if(exitCode !=0)
+                {
+                        printf("Process exited with exit code", exitCode);
+                        return -1;
+                }
+        }
+        else if(WIFSIGNALED(stat))
+        {
+                printf("Process terminated due to signal", WTERMSIG(stat));
+                return -1;
+        }
+        else if(WIFSTOPPED(stat))
+        {
+                printf("Process stopped due to signal %d", WSTOPSIG(stat));
+                return -1;
+        }
+
+        return 0;
+}
+
+void makeUpper(char* buff, size_t n)
+{
+        for(size_t i = 0; i < n; ++i)
+
+                buff[i] = toupper(buff[i]);
+}
+
+int main()
+{
+        FILE* in = popen("cat input.txt", "r");
+        if(in == NULL)
+        {
+                perror("Error in opening pipe");
+                return 1;
+        }
+
+
+        size_t count;
+        char buff[MAXBUFF];
+        while(1)
+        {
+                count = fread(buff, sizeof(char), MAXBUFF, in);
+                if(ferror(in))
+                {
+                        if(errno == EINTR)
+                        {
+                                errno = 0;
+                                continue;
+                        }
+                        perror("Error in reading from pipe");
+                        if(pclose(in) == -1)
+                                perror("Error in closing pipe");
+                        return 1;
+                }
+                break;
+        }
+
+        int stat = pclose(in);
+        if(stat == -1)
+        {
+                perror("Error in closing pipe");
+                return -1;
+        } else if(!WIFEXITED(stat))
+        {
+                printStat(stat);
+                return 1;
+        }
+
+        makeUpper(buff, count);
+
+        while(1)
+        {
+                fwrite(buff, sizeof(char), count, stdout);
+                if(ferror(stdout))
+                {
+                        if(errno == EINTR)
+                        {
+                                errno = 0;
+                                continue;
+                        }
+                        perror("Error in writing to stdout");
+                        return 1;
+                }
+                break;
+        }
+        return 0;
+}
