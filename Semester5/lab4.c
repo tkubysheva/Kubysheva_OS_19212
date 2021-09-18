1479bcb6f8cbf40cf15867f0ed39ed9a4678ec07
Tanya <70838088+tkubysheva@users.noreply.github.com>
2021-09-18 14:13:44 +0700

    Create lab4.c

---
+++ Semester5/lab4.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void * thread_body(void * param) {
+     while(1){
+        printf("Hello, I'm child!");
+     }
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t thread;
+    int thread_creating_code = pthread_create(&thread, NULL, thread_body, NULL);
+    if (thread_creating_code != 0) {
+        printf("Error in  creating thread: error_code: %d\n", thread_creating_code);
+        return (EXIT_FAILURE);
+    }
+    sleep(2);
+    int thread_cancel_code = pthread_cancel(thread);
+    if (thread_cancel_code != 0) {
+        printf("Error in canceling thread: error_code: %d\n", thread_cancel_code);
+        return (EXIT_FAILURE);
+    }
+
+    return (EXIT_SUCCESS);
+}                                                                                                     
