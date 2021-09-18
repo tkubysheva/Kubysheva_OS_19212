6c969ac43f676258736949b6cbfcf52648c25f7f
Tanya <70838088+tkubysheva@users.noreply.github.com>
2021-09-18 13:17:19 +0700

    Update lab3.c

--- Semester5/lab3.c
+++ Semester5/lab3.c
@@ -10,16 +10,22 @@ struct strings{
 };
 
 void * thread_body(struct strings* param) {
-     for(int i = 1; i < param->num_of_lines; i++){
+     for(int i = 0; i < param->num_of_lines; i++){
         printf("%s ", param->lines[i]);
      }
 }
 
 int main(int argc, char *argv[]) {
     pthread_t thread1, thread2, thread3, thread4;
-    char* s1 = "aaa", s2 = "hello!", s3 = "I have an apple", s4 = "LOL";  
-    char* a[] = {s1, s2, s3};
-    struct strings param1 =  {3, a};
+    char* s1 = "aaa";
+    char* s2 = "hello!";
+    char* s3 = "I have an apple";
+    char* s4 = "LOL";
+    char* strs1[] = {s1, s2, s3};
+    char* strs2[] = {s4, s4, s1, s1, s2};
+    char* strs3[] = {s2, s1, s4, s2};
+    char* strs4[] = {s1, s3};
+    struct strings param1 =  {3, strs1};
     int thread1_creating_code = pthread_create(&thread1, NULL, thread_body, &param1);
     if (thread1_creating_code != 0) {
         printf("Error in  creating thread: error_code: %d\n", thread1_creating_code);
