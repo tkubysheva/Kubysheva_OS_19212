2bfd2b3738bdd2985f1a9facfe1d12684cf1c81d
Tanya <70838088+tkubysheva@users.noreply.github.com>
2021-10-12 00:11:55 +0700

    Update lab3.c

--- Semester5/lab3.c
+++ Semester5/lab3.c
@@ -2,26 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <string.h>
 
 struct strings{
-     int num_of_lines;
-     char** lines;
+    int num_of_lines;
+    char** lines;
 };
 
 void * thread_body(void* par) {
-     struct strings* param = par;
-     for(int i = 0; i < param->num_of_lines; i++){
-        printf("%s ", param->lines[i]);
-        free(param->lines[i]);
-     }
-     free(param->lines);
-     free(param);
-     pthread_exit(NULL);
+    if(par == NULL){
+        return NULL;
+    }
+    struct strings param = *(struct strings*)par;
+    for(int i = 0; i < param.num_of_lines; i++){
+        printf("%s ", param.lines[i]);
+        free(param.lines[i]);
+    }
+    free(param.lines);
+    free(par);
+    return NULL;
 }
 
 int creating_thread(pthread_t thread, struct strings* param){
-    int thread_creating_code = pthread_create(&thread, NULL, thread_body, &param);
+    int thread_creating_code = pthread_create(&thread, NULL, thread_body, param);
     if (thread_creating_code != 0) {
         printf("Error in  creating thread: error_code: %d\n", thread_creating_code);
         return 0;
@@ -38,32 +40,32 @@ int main(int argc, char *argv[]) {
     char** strs1 = (char**)malloc(sizeof(char*)*3);
     for(int i = 0; i < 3; ++i){
         strs1[i] = (char*)malloc(sizeof(char)*4);
-        strs1[i] = "aaa";        
+        strs1[i] = "aaa";
     }
     char** strs2 = (char**)malloc(sizeof(char*)*5);
     for(int i = 0; i < 5; ++i){
-        strs1[i] = (char*)malloc(sizeof(char)*7);
-        strs1[i] = "hello!";        
+        strs2[i] = (char*)malloc(sizeof(char)*7);
+        strs2[i] = "hello!";
     }
     char** strs3 = (char**)malloc(sizeof(char*)*4);
     for(int i = 0; i < 4; ++i){
-        strs1[i] = (char*)malloc(sizeof(char)*11);
-        strs1[i] = "good night";        
+        strs3[i] = (char*)malloc(sizeof(char)*11);
+        strs3[i] = "good night";
     }
     char** strs4 = (char**)malloc(sizeof(char*)*2);
     for(int i = 0; i < 2; ++i){
-        strs1[i] = (char*)malloc(sizeof(char)*17);
-        strs1[i] = "Where is my bag?";        
+        strs4[i] = (char*)malloc(sizeof(char)*17);
+        strs4[i] = "Where is my bag?";
     }
     struct strings* param = (struct strings*) malloc(sizeof(struct strings)*4);
-    param[0] = {3, strs1};
-    param[1] = {5, strs2};
-    param[2] = {4, strs3};
-    param[3] = {2, strs4};
+    param[0].num_of_lines = 3; param[0].lines= strs1;
+    param[1].num_of_lines = 5; param[1].lines= strs2;
+    param[2].num_of_lines = 4; param[2].lines= strs3;
+    param[3].num_of_lines = 2; param[3].lines= strs4;
     for(int i = 0; i < 4; i++){
-         if (creating_thread(threads[i], &param[i]) == 0) {
-              return (EXIT_FAILURE);
-         }
+        if (creating_thread(threads[i], &param[i]) == 0) {
+            return (EXIT_FAILURE);
+        }
     }
     pthread_exit(NULL);
     return (EXIT_SUCCESS);
